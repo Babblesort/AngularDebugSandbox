fc471433f6cb8461de2da3268609c210f5a67c39
Paul McConnell <paul.mcconnell@asynchrony.com>
2017-02-25 13:03:44 -0600

    create basic grunt build and watch tasks

---
+++ gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    eslint: {
+      src: ['src/**/*.js'],
+    },
+    concat: {
+      options: {
+        internal: {
+          sourceMap: true
+        }
+      },
+      internal: {
+        src: ['src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      },
+      vendor: {
+        src: [
+          'node_modules/angular/angular.js',
+        ],
+        dest: 'dist/vendor.js'
+      }
+    },
+    copy: {
+      index: {
+        files: [
+          {
+            cwd: 'src',
+            expand: true,
+            src: ['index.html'],
+            dest: 'dist',
+          }
+        ]
+      }
+    },    
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.internal.dest %>'],
+          'dist/vendor.min.js': ['<%= concat.vendor.dest %>']
+        }
+      }
+    },
+    watch: {
+      files: ['src/**/*.js', 'src/*.html'],
+      tasks: ['build']
+    }
+  });
+
+  grunt.loadNpmTasks('gruntify-eslint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['eslint']);
+  grunt.registerTask('build', ['eslint', 'concat', 'copy', 'uglify']);
+  grunt.registerTask('serve', ['build', 'watch']);
+};
