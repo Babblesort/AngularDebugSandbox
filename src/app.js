6a3bc894b75e0b15dfea0ad4a9df1847935e4f49
Paul McConnell <paul.mcconnell@asynchrony.com>
2017-02-25 22:40:05 -0600

    serve a very simple angular app

---
+++ src/app.js
@@ -0,0 +1,10 @@
+angular.module('angulardebugsandbox', [])
+.run(() => {})
+.controller('SumController', $scope => {
+  $scope.data = {
+    num1: 0,
+    num2: 0,
+    sum: 0,
+  };
+  $scope.onNumChange = () => $scope.data.sum = parseInt($scope.data.num1, 10) + parseInt($scope.data.num2, 10);
+});
