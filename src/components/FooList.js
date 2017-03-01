0a18e8ed291c8ec34264dd895b24c40d92c2f01d
Paul McConnell <paul.mcconnell@asynchrony.com>
2017-02-28 20:58:18 -0600

    wrap row component with list component

---
+++ src/components/FooList.js
@@ -0,0 +1,7 @@
+angular.module('sandbox').component('fooList', {
+  bindings: {
+      rows: '<'
+    },
+    controllerAs: 'model',
+    template: '<div ng-repeat="row in model.rows"><foo-row row-data="row"></foo-row></div>'
+});
