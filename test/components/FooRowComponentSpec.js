f254bb4c932722b5c12ace3188819ee385fe5e64
Paul McConnell <paul.mcconnell@asynchrony.com>
2017-03-01 21:49:00 -0600

    prove babel transpile happening on test source files

--- test/components/FooRowComponentSpec.js
+++ test/components/FooRowComponentSpec.js
@@ -1,10 +1,10 @@
-describe('Component: FooRow', function () {
+describe('Component: FooRow', () => {
   beforeEach(module('sandbox'));
 
   let element;
   let scope;
 
-  beforeEach(inject(function ($rootScope, $compile) {
+  beforeEach(inject(($rootScope, $compile) => {
     scope = $rootScope.$new();
     element = angular.element('<foo-row row-data="someRowData"></foo-row>');
     element = $compile(element)(scope);
@@ -16,12 +16,12 @@ describe('Component: FooRow', function () {
     scope.$digest();
   }));
 
-  it('should render the text', function () {
+  it('should render the text', () => {
     let div = element.find('div');
     expect(div.text()).to.equal('1-widget-666');
   });
 
-  it('should pass', function () {
+  it('should pass', () => {
     expect(true).to.equal(true);
   });
 });
