4709abda9f6ec573839546bf5b1fe6e1ae3373c4
Paul McConnell <paul.mcconnell@asynchrony.com>
2017-03-03 09:27:56 -0600

    do not launch chrome when karma server starts

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     autoWatchBatchDelay: 300,
-    browsers: ['Chrome'],
+    browsers: [],
     client: {
       mocha: {
         reporter: 'html'
