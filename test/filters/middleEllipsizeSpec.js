19ca164e770329a11a6441cef2ca76fa9e8e7721
Paul McConnell <paul.mcconnell@asynchrony.com>
2017-03-03 17:52:02 -0600

    rename object under test for readability

--- test/filters/middleEllipsizeSpec.js
+++ test/filters/middleEllipsizeSpec.js
@@ -1,40 +1,40 @@
 describe('Filter: middleEllipsize', function () {
 
-  let middleEllipsizeFilter;
+  let filter;
   let beforeEachSetup = () => {
     module('sandbox');
-    inject(_middleEllipsizeFilter_ => middleEllipsizeFilter = _middleEllipsizeFilter_);
+    inject(_middleEllipsizeFilter_ => filter = _middleEllipsizeFilter_);
   }
   beforeEach(beforeEachSetup);
 
   it('returns text unchanged if text length less <= maxCharacters', function () {
-    expect(middleEllipsizeFilter('abc', 4)).to.equal('abc');
-    expect(middleEllipsizeFilter('abcd', 4)).to.equal('abcd');
+    expect(filter('abc', 4)).to.equal('abc');
+    expect(filter('abcd', 4)).to.equal('abcd');
   });
 
   it('returns empty string if text is null or undefined', function () {
-    expect(middleEllipsizeFilter(null, 4)).to.equal('');
-    expect(middleEllipsizeFilter(undefined, 4)).to.equal('');
+    expect(filter(null, 4)).to.equal('');
+    expect(filter(undefined, 4)).to.equal('');
   });
 
   it('returns text changed if text length > maxCharacters', function () {
-    expect(middleEllipsizeFilter('abcde', 4)).to.equal('a...');
-    expect(middleEllipsizeFilter('abcdefg', 5)).to.equal('a...g');
-    expect(middleEllipsizeFilter('abcdefgh', 7)).to.equal('ab...gh');
+    expect(filter('abcde', 4)).to.equal('a...');
+    expect(filter('abcdefg', 5)).to.equal('a...g');
+    expect(filter('abcdefgh', 7)).to.equal('ab...gh');
   });
 
   it('favors front half of text when peforming ellipsis and text cannot be divided evenly', function () {
-    expect(middleEllipsizeFilter('abcdefgh', 6)).to.equal('ab...h');
-    expect(middleEllipsizeFilter('abcdefghi', 8)).to.equal('abc...hi');
+    expect(filter('abcdefgh', 6)).to.equal('ab...h');
+    expect(filter('abcdefghi', 8)).to.equal('abc...hi');
   });
 
   it('uses custom ellipsisText if passed in', function () {
-    expect(middleEllipsizeFilter('abcdefgh', 6, '!!')).to.equal('ab!!gh');
-    expect(middleEllipsizeFilter('abcdefghi', 7, '!!')).to.equal('abc!!hi');
+    expect(filter('abcdefgh', 6, '!!')).to.equal('ab!!gh');
+    expect(filter('abcdefghi', 7, '!!')).to.equal('abc!!hi');
   });
 
   it('returns text unchanged if ellipsisText length <= maxCharacters', function () {
-    expect(middleEllipsizeFilter('abcd', 3)).to.equal('abcd');
-    expect(middleEllipsizeFilter('abcd', 4, '!-!-')).to.equal('abcd');
+    expect(filter('abcd', 3)).to.equal('abcd');
+    expect(filter('abcd', 4, '!-!-')).to.equal('abcd');
   });
 });
